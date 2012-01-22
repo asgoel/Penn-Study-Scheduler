78f3b81301a822082c5e1feb0853fea6635ff0ab
Max Scheiber <keasbeynumb@gmail.com>
2012-01-21 19:36:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PennStudyScheduler::Application.config.secret_token = 'b05f7514ea3e838aef8fbce61062e0cc471f772a9ce1d72ef7e5fa0a7fcd522e6ba99942d786fd8e9a53f2510df0f64d20d7364d1c53c419cf744fef5e2a25b0'
