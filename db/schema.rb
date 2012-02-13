47f28f8f7c927273225d37440dd946e4c5103384
Ashu Goel <soxfan981@gmail.com>
2012-02-12 19:55:44 -0500

    edited gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20120123024220) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
