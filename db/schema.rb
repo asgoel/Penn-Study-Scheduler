60f0f92182d4152b6287c27e4c542aeb40b2ee1d
Ashu Goel <soxfan981@gmail.com>
2012-02-19 17:05:18 -0500

    form styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213062944) do
+ActiveRecord::Schema.define(:version => 20120217063324) do
 
   create_table "courses", :force => true do |t|
     t.string   "department"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120213062944) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
@@ -27,6 +28,8 @@ ActiveRecord::Schema.define(:version => 20120213062944) do
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
